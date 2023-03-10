a2e295a115ffe9bcac00e2e55ed57dc7eceb2f37
Thiago Ribeiro <thiago.bernardes@aluno.ifsp.edu.br>
2023-03-10 01:18:08 -0300

    Versão inicial - suporte portugues

--- jpg2txt.py
+++ jpg2txt.py
@@ -2,6 +2,9 @@ import pytesseract
 import cv2
 
 # passo 1: ler a imagem
+imagem = cv2.imread("media/teste.png")
 
 # passo 2: pedir pro tessaract extrair o texto da imagem
+texto = pytesseract.image_to_string(imagem, lang="por")
 
+print(texto)
